93f9c004763ea259c71dc518b84829e59c1290e3
Zuro <darckProyect8@gmail.com>
2025-09-25 17:19:48 -0500

    Faltaba el componente de header en el main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,18 @@ import { RouterOutlet } from '@angular/router';
 import { OAuthModule } from 'angular-oauth2-oidc';
 import { LoginDrawerComponent } from './features/auth/components/login-drawer/login-drawer.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, OAuthModule, FooterComponent, LoginDrawerComponent],
+  imports: [
+    RouterOutlet,
+    OAuthModule,
+    FooterComponent,
+    LoginDrawerComponent,
+    HeaderComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
